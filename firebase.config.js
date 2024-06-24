2e466b6d5b957b9560b6b2cb10e6428bc6cc876e
muhammad-2002 <masumbilla10104@gmail.com>
2024-06-24 23:42:17 +0600

    last commit

--- firebase.config.js
+++ firebase.config.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_APIKEY, 
-  authDomain:import.meta.env.VITE_AUTHDOMAIN, 
-  projectId:import.meta.env.VITE_PROJECTID, 
-  storageBucket:import.meta.env.VITE_STORAGEBUCKET, 
-  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID, 
-  appId:import.meta.env.VITE_APPID, 
-  measurementId:import.meta.env.VITE_MEASUREMENTID, 
+  apiKey: "AIzaSyBF04HCVIVnvAoAklMxKi6zW-HAhkPKixE",
+  authDomain: "logistics-international-7d03b.firebaseapp.com",
+  projectId: "logistics-international-7d03b",
+  storageBucket: "logistics-international-7d03b.appspot.com",
+  messagingSenderId: "77385030417",
+  appId: "1:77385030417:web:618c7f61fcc1b98cd98ab0",
+  measurementId: "G-73CXB9ZSWY"
 };
 
 
